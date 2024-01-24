45ea900910e7339973ebd5b808acadf321bf303c
Pratik Devnal <79634014+pratikdevnal@users.noreply.github.com>
2024-01-24 18:06:51 +0530

    Modified App.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  <div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+      <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+        {/* theme button  */}
+      </div>
 
-  return <></>;
+      <div className="w-full max-w-sm mx-auto">{/* card  */}</div>
+    </div>
+  </div>;
 }
 
 export default App;
